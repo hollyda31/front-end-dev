37d90185ab14337902f73e7b49d41047630f2b20
Holly Antle <hollyda31@gmail.com>
2022-07-25 22:36:34 -0500

    Added new blank pages

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Layout() {
+	return <h1>this is the layout</h1>;
+}
+
+export default Layout;
---
+++ src/pages/Skills.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Skills() {
+	return <h1>this is the skills</h1>;
+}
+
+export default Skills;
